ab2d3c91a42913ab33a3237b0c799a0b48dd3e4e
adrsri001 <adarshshrivastav08@gmail.com>
2025-07-30 07:07:13 +0530

    Update dep3.py

--- dep3.py
+++ dep3.py
@@ -1,15 +1,11 @@
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
-from matplotlib import pyplot as plt
 import warnings
 import pickle
 warnings.filterwarnings("ignore")
 df=pd.read_excel(r"C:\Users\adars\Downloads\Multi_Data.xlsx")
-# print(df)
 x=df[["area","bedrooms","age"]]
 y=df.price
-# print(x)
-# print(y)
 m1=LinearRegression()
 m1.fit(x,y) 
 predict=m1.predict([[12000,5,6]])
